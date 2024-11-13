c31902cd1b9ac381279ae3630c12b83c01637ced
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-11-13 11:09:02 +0100

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: "https://impucky.github.io",
+  site: "https://raphael-beaugiraud.dev",
 });
